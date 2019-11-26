41d903936c5e47e15755516c9c3bd6ee66ae0642
JoshuaChin95 <38680084+JoshuaChin95@users.noreply.github.com>
2019-11-26 19:27:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const axios = require('axios');
 const Movie = require('./Movie');
 
 const apikey = 'd554a35b';
+
+const port = process.env.PORT || 5000;
+
+//--- heroku ---
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 //localhost:5000/getmovie?title=MovieTitle
 app.get('/getmovie', (req, res) => {
@@ -61,6 +72,6 @@ app.get('/deletemovie', (req, res) => {
     });
 });
 
-app.listen(5000, () => {
-  console.log('server listening on port 5000');
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
 });
